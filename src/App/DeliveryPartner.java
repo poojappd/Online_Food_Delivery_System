4517afe05a9590912630e7295e47e39f5ab0a30c
poojappd <poojapriyadarshnips@gmail.com>
2022-05-16 21:05:26 +0530

    ch p1

--- src/App/DeliveryPartner.java
+++ src/App/DeliveryPartner.java
@@ -7,33 +7,48 @@ class DeliveryPartner {
     private String deliveryPartnerName;
     private int deliveryPartnerAge;
     private String currentArea;
+    private String primaryArea;
     private ArrayList<DeliveryPackage> deliveryPackages;
     private ArrayList<String> deliveringAreas = new ArrayList<>();
 
-    DeliveryPartner(String deliveryPartnerName, int deliveryPartnerAge){
+    DeliveryPartner(String deliveryPartnerName, int deliveryPartnerAge, String primaryArea ){
         this.deliveryPartnerName = deliveryPartnerName;
         this.deliveryPartnerAge = deliveryPartnerAge;
+        this.primaryArea = primaryArea;
+        this.currentArea = primaryArea;
         this.deliveryPartnerId = IdGenerator.generateDeliveryPartnerId();
+    }
+
+    void addDeliveringAreas(String area){
+        deliveringAreas.add(area);
     }
 
     int getDeliveryPartnerId(){
         return deliveryPartnerId;
     }
 
+    String getCurrentArea(){
+        return currentArea;
+    }
+
     void updateCurrentArea(String updatedArea){
         this.currentArea = updatedArea;
     }
 
+    private void addToDeliveryPackage(DeliveryPackage deliveryPackage){
+        deliveryPackages.add(deliveryPackage);
+    }
+
     void pickupOrder(Restaurant currentRestaurant, String userName,
                      String userArea){
-        currentRestaurant.deliveryPartnerPickup(userName);
+        ArrayList<Restaurant.Food> foodItems = currentRestaurant.deliveryPartnerPickup(userName);
+        DeliveryPackage newDeliveryPackage = new DeliveryPackage(userName, userArea, foodItems);
+        addToDeliveryPackage(newDeliveryPackage);
     }
 
     void deliverOrder(){
         //remove package from deliveryPackages
     }
-    void addDeliveringAreas(String area){
-        deliveringAreas.add(area);
-    }
+    
 
 }
