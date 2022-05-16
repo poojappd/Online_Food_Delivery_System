0b65cd95f806ba9b604dbafb5431585a97b39533
poojappd <poojapriyadarshnips@gmail.com>
2022-05-16 21:06:17 +0530

    ch p1

--- src/App/DeliveryPackage.java
+++ src/App/DeliveryPackage.java
@@ -8,9 +8,9 @@ class DeliveryPackage {
     int deliveryId;
     ArrayList<Restaurant.Food> foodItems;
 
-    DeliveryPackage(String userName, String userLocation, ArrayList<Restaurant.Food> foodItems) {
+    DeliveryPackage(String userName, String userArea, ArrayList<Restaurant.Food> foodItems) {
         this.userName = userName;
-        this.userLocation = userLocation;
+        this.userArea = userArea;
         this.foodItems = foodItems;
         this.deliveryId = IdGenerator.generateDeliveryPackageId();
     }
